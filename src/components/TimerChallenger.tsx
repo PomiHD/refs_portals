482a56b75b742d58ae774637c28ab3dffd671ad6
PomiHD <wsgddjy@live.com>
2024-02-05 20:08:17 +1000

    139. use `forwardRef`Forwarding Refs to Custom Components...

--- src/components/TimerChallenger.tsx
+++ src/components/TimerChallenger.tsx
@@ -1,14 +1,29 @@
 import { useRef, useState } from "react";
 import ResultModel from "./ResultModel";
 
+/**
+ * A timer challenge component
+ * @param title - The title of the challenge
+ * @param targetTime - The target time of the challenge
+ * @return {*} - The timer challenge component
+ */
 export default function TimerChallenger({ title, targetTime }) {
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
+  
+  /** @type {React.MutableRefObject<NodeJS.Timeout | null>}
+   * A reference to the timer
+   */
   const timer = useRef<NodeJS.Timeout | null>();
+  /** @type {React.MutableRefObject<HTMLDialogElement | null>}
+   * A reference to the dialog element
+   */
+  const dialog = useRef<HTMLDialogElement | null>();
 
   function handleStart() {
     timer.current = setTimeout(() => {
       setTimerExpired(true);
+      dialog.current.showModal();
     }, targetTime * 1000);
     setTimerStarted(true);
   }
@@ -19,7 +34,7 @@ export default function TimerChallenger({ title, targetTime }) {
 
   return (
     <>
-      {timerExpired && <ResultModel result="lost" targetTime={targetTime} />}
+      <ResultModel ref={dialog} result="lost" targetTime={targetTime} />
       <section className={"challenge"}>
         <h2>{title}</h2>
         {timerExpired && <p>You lost!</p>}
