e11d50ba84416b29bcc842405a6bf343c8408258
PomiHD <wsgddjy@live.com>
2024-02-05 14:32:51 +1000

    Connecting & Accessing HTML Elements via Refs but not two way binding

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,23 +1,20 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function Player() {
   const [enteredPlayerName, setEnteredPlayerName] = useState("");
-  const [submitted, setSubmitted] = useState(false);
-
-  function handleChange(event) {
-    setSubmitted(false);
-    setEnteredPlayerName(event.target.value);
-  }
+  const playerName = useRef(null);
 
   function handleClick() {
-    setSubmitted(true);
+    setEnteredPlayerName(playerName.current.value);
   }
 
   return (
     <section id="player">
-      <h2>Welcome {submitted ? enteredPlayerName : "unknown entity"}</h2>
+      <h2>
+        Welcome {enteredPlayerName ? enteredPlayerName : "unknown entity"}
+      </h2>
       <p>
-        <input onChange={handleChange} value={enteredPlayerName} type="text" />
+        <input ref={playerName}  type="text" />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
